bbfef5371553e27415c546e98971b75eb4c7fd60
Saraswati121 <99686266+Saraswati121@users.noreply.github.com>
2022-07-22 03:08:49 +0530

    done

---
+++ index.js
@@ -0,0 +1,22 @@
+const express=require("express")
+const userRouterers = require("./routes/person")
+const userRoute = require("./routes/user")
+const connections= require("./storage/db")
+const app=express()
+const cors= require("cors")
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors({
+    origin:["http://localhost:3000"]
+}))
+
+app.use("/user", userRoute)
+app.use("/profile",userRouterers)
+
+const port = process.env.port || 8080
+
+app.listen(port,async ()=>{
+    await connections
+   console.log("server started")
+})
---
+++ routes/user.js
@@ -0,0 +1,24 @@
+const {Router} = require("express")
+const userRoute= Router()
+const user= require("../models/user")
+
+userRoute.post("/signup",(req,res)=>{
+    const User= new user(req.body)
+    User.save((err,success)=>{
+        if(err){
+            return res.status(500).send({message:"Error occured"})
+        }
+        return res.status(201).send({message:"signup successful", User:success._doc})
+    })
+})
+
+userRoute.post("/login",async(req,res)=>{
+    const {email, password} = req.body;
+    const validUser= await user.find({email,password})
+    if(validUser.length < 1){
+        return res.status(401).send({message:"Invalid Credentials"})
+    }
+    return res.send(validUser)
+})
+
+module.exports= userRoute
